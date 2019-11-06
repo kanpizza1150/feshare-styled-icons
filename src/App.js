4ea23f48ebd7a2c6a475f132f767ce21f7ef9c7f
kanpichcha Nokdam <knokd@sertiscorp.com>
2019-11-07 03:13:27 +0700

    Add puppys' hand

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 import {Lock} from 'styled-icons/material'
 import {Zap} from 'styled-icons/octicons/Zap'
+import {Baidu} from 'styled-icons/boxicons-logos/Baidu'
+
 export const LockIcon = styled(Lock)`
   color: ${props => (props.important ? 'red' : 'blue')};
   height: 100px;
@@ -9,8 +11,21 @@ export const LockIcon = styled(Lock)`
 export const ZapIcon = styled(Zap)`
   color: ${props => (props.important ? 'pink' : 'orange')};
   height: 50px;
+  padding: 20px;
+  transition: 0.3s all;
+
   &:hover {
     color: ${props => (props.important ? 'red' : 'blue')};
+    transform: scale(2) rotate(360deg);
+  }
+`
+
+export const BaiduIcon = styled(Baidu)`
+  color: ${props => props.primaryColor};
+  transition: 0.5s all;
+  &:hover {
+    transform: scale(1.2) translateY(5px);
+    color: ${props => props.secondaryColor};
   }
 `
 class App extends React.Component {
@@ -20,7 +35,51 @@ class App extends React.Component {
         <LockIcon important={true} />
         <LockIcon important={false} />
         <ZapIcon important={false} />
-        <ZapIcon important={true} />
+        <ZapIcon important={true} title="Zap" />
+        <div>
+          <BaiduIcon
+            size="80px"
+            title="puppy"
+            primaryColor="purple"
+            secondaryColor="red"
+          />
+          <BaiduIcon
+            size="80px"
+            title="puppy"
+            primaryColor="cyan"
+            secondaryColor="orange"
+          />
+          <BaiduIcon
+            size="80px"
+            title="puppy"
+            primaryColor="blue"
+            secondaryColor="yellow"
+          />
+          <BaiduIcon
+            size="80px"
+            title="puppy"
+            primaryColor="green"
+            secondaryColor="green"
+          />
+          <BaiduIcon
+            size="80px"
+            title="puppy"
+            primaryColor="yellow"
+            secondaryColor="blue"
+          />
+          <BaiduIcon
+            size="80px"
+            title="puppy"
+            primaryColor="orange"
+            secondaryColor="cyan"
+          />
+          <BaiduIcon
+            size="80px"
+            title="puppy"
+            primaryColor="red"
+            secondaryColor="purple"
+          />
+        </div>
       </>
     )
   }
